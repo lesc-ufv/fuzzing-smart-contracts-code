62aa489893334c45271c7ca7a73dab14d1d54fda
aline <145055918+alinec-santos@users.noreply.github.com>
2024-10-27 17:45:09 -0300

    Update tests_aline.py
    inserção das funções para detecção de vulnerabilidade de reentrada

--- tests_aline/tests_aline.py
+++ tests_aline/tests_aline.py
@@ -1,67 +1,130 @@
-from web3 import Web3
-import json
 import solcx
-import networkx as nx
-import matplotlib.pyplot as plt
-import random
-from z3 import *
+from web3 import Web3
+import random, pprint
+import json
 
-# Conectando ao Hardhat
-hardhat_url = "http://127.0.0.1:8545"
-w3 = Web3(Web3.HTTPProvider(hardhat_url))
 
-# Verifique a conexão
-if w3.is_connected():
-    print("Conectado ao Hardhat!")
-else:
-    print("Falha ao conectar ao Hardhat")
+# Classe para detectar reentrância
+class ReentrancyDetector():
+    def __init__(self):
+        self.init()
 
-# Usando a primeira conta do Hardhat
-w3.eth.default_account = w3.eth.accounts[0]
+    def init(self):
+        self.swc_id = 107
+        self.severity = "High"
+        self.sloads = {}
+        self.calls = set()
 
-# Funções para o fuzzing
+    def detect_reentrancy(self, tainted_record, current_instruction, transaction_index):
+        # Lembrar sloads
+        if current_instruction["op"] == "SLOAD":
+            if tainted_record and tainted_record.stack and tainted_record.stack[-1]:
+                storage_index = convert_stack_value_to_int(current_instruction["stack"][-1])
+                self.sloads[storage_index] = current_instruction["pc"], transaction_index
+        # Lembrar chamadas com mais de 2300 gas e onde o valor é maior que zero
+        elif current_instruction["op"] == "CALL" and self.sloads:
+            gas = convert_stack_value_to_int(current_instruction["stack"][-1])
+            value = convert_stack_value_to_int(current_instruction["stack"][-3])
+            if gas > 2300 and (value > 0 or (tainted_record and tainted_record.stack and tainted_record.stack[-3])):
+                self.calls.add((current_instruction["pc"], transaction_index))
+            if gas > 2300 and (tainted_record and tainted_record.stack and tainted_record.stack[-2]):
+                self.calls.add((current_instruction["pc"], transaction_index))
+                for pc, index in self.sloads.values():
+                    if pc < current_instruction["pc"]:
+                        return current_instruction["pc"], index
+        # Verificar se este sstore está acontecendo após uma chamada
+        elif current_instruction["op"] == "SSTORE" and self.calls:
+            if tainted_record and tainted_record.stack and tainted_record.stack[-1]:
+                storage_index = convert_stack_value_to_int(current_instruction["stack"][-1])
+                if storage_index in self.sloads:
+                    for pc, index in self.calls:
+                        if pc < current_instruction["pc"]:
+                            return pc, index
+        # Limpar sloads e calls de transações anteriores
+        elif current_instruction["op"] in ["STOP", "RETURN", "REVERT", "ASSERTFAIL", "INVALID", "SUICIDE", "SELFDESTRUCT"]:
+            self.sloads = {}
+            self.calls = set()
+        return None, None
+
+def convert_stack_value_to_int(value):
+    return int(value, 16)
+
+def compile_smartcontract(compiler_version, contract_filename, source_code):
+    if compiler_version not in solcx.get_installed_solc_versions():
+        solcx.install_solc(compiler_version)
+    solcx.set_solc_version(solc_version, True)
+
+    compiler_output = solcx.compile_standard({
+        'language': 'Solidity',
+        'sources': {contract_filename: {'content': source_code}},
+        'settings': {
+            "optimizer": {"enabled": True, "runs": 200},
+            "evmVersion": "cancun",
+            "outputSelection": {
+                contract_filename: {
+                    "*": [
+                        "abi",
+                        "evm.deployedBytecode",
+                        "evm.bytecode.object",
+                        "evm.legacyAssembly",
+                    ],
+                }
+            }
+        }
+    }, allow_paths='.')
+    print("Smart contract compiled!")
+
+    return compiler_output
+
+def connect_in_blockchain(url):
+    w3 = Web3(Web3.HTTPProvider(url))
+    if w3.is_connected():
+        print("Blockchain connected succesfully!")
+        w3.eth.default_account = w3.eth.accounts[0]
+        return w3
+    
+    print("Error during the connection with the blockchain!")
+    return None
+
+def deploy_smartcontract(w3, abi, bytecode):
+    smart_contract = w3.eth.contract(abi=abi, bytecode=bytecode)
+    tx_hash = smart_contract.constructor().transact()
+    tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
+    contract_address = tx_receipt.contractAddress
+    print(f"Contract Address: {contract_address}")
+    
+    return w3.eth.contract(address=contract_address, abi=abi)
+
 def get_pcs_and_jumpis(bytecode):
     pcs = [i for i in range(len(bytecode))]
-    jumpis = [i for i in range(len(bytecode)) if bytecode[i:i+2] == '56']  # Exemplo: '56' é o opcode JUMPI
+    jumpis = [i for i in range(len(bytecode)) if bytecode[i:i+2] == '56']  # Example: '56' is JUMPI opcode
     return pcs, jumpis
 
-def build_cfg(pcs, jumpis):
-    G = nx.DiGraph()  # Grafo dirigido para o CFG
-    for i in range(len(pcs) - 1):
-        G.add_node(pcs[i], label=f'PC: {pcs[i]}')
-        G.add_edge(pcs[i], pcs[i + 1], label="next")
-    for jump in jumpis:
-        target_pc = jump + 2 if (jump + 2 < len(pcs)) else jump
-        G.add_edge(jump, target_pc, label="jump")
-    return G
+def simulate_transaction(w3, contract, function_name, inputs=None, value=0):
+    try:
+        if inputs:
+            # Sorting inputs accordingly with function parameters
+            sorted_inputs = [inputs[param['name']] for param in contract.functions[function_name].abi['inputs']]
+            txn = getattr(contract.functions, function_name)(*sorted_inputs).transact({'value': value})
+        else:
+            txn = getattr(contract.functions, function_name)().transact({'value': value})
+        tx_receipt = w3.eth.wait_for_transaction_receipt(txn)
+        print(f"Transaction '{function_name}' executed successfully: {tx_receipt.transactionHash.hex()}")
+        return tx_receipt
+    except Exception as e:
+        print(f"Error during transaction '{function_name}' execution: {e}")
+        return None
 
-def plot_cfg(G):
-    pos = nx.spring_layout(G)
-    labels = nx.get_edge_attributes(G, 'label')
-    node_labels = nx.get_node_attributes(G, 'label')
-    nx.draw(G, pos, with_labels=True, labels=node_labels, node_size=2000, node_color='lightblue', font_size=10, font_color='black')
-    nx.draw_networkx_edge_labels(G, pos, edge_labels=labels)
-    plt.title('Control Flow Graph')
-    plt.savefig('grafo_de_controle_de_fluxo.png')
-    print("Gráfico gerado: grafo_de_controle_de_fluxo.png")
-
-def generate_random_inputs(abi, contract_instance):
+def generate_random_inputs(abi):
     inputs = []
+    
     for item in abi:
-        if item['type'] == 'function':
-            function_inputs = {}
+        if item['type'] == 'function' and item['name'] != 'balances': # Only for EtherStore!
+            function_inputs = dict()
             for input_param in item.get('inputs', []):
                 param_type = input_param['type']
                 if param_type == 'uint256':
-                    if item['name'] == 'withdraw':
-                        # Gera um valor até o saldo disponível para evitar reverts por saldo insuficiente
-                        current_balance = contract_instance.functions.getBalance().call()
-                        if current_balance > 0:
-                            value = random.randint(0, current_balance)
-                        else:
-                            value = 0
-                    else:
-                        value = random.randint(0, 2**256 - 1)
+                    value = random.randint(0, 2**256 - 1)
                 elif param_type == 'address':
                     value = '0x' + ''.join(random.choices('0123456789abcdef', k=40))
                 elif param_type == 'string':
@@ -72,134 +135,103 @@ def generate_random_inputs(abi, contract_instance):
                     size = int(param_type.replace('bytes', '')) if len(param_type) > 5 else random.randint(1, 32)
                     value = '0x' + ''.join(random.choices('0123456789abcdef', k=size*2))
                 else:
-                    value = None  # Tipos não suportados podem ser ignorados ou tratados conforme necessário
+                    value = None # Non-supported types can be ignored or treated as needed
                 if value is not None:
                     function_inputs[input_param['name']] = value
             inputs.append({
+                'stateMutability': item["stateMutability"],
                 'name': item['name'],
                 'inputs': function_inputs
             })
+
     return inputs
 
-def simulate_transaction(contract, function_name, inputs=None, value=0):
-    try:
-        if inputs:
-            # Ordena os inputs de acordo com a ordem dos parâmetros na função
-            sorted_inputs = [inputs[param['name']] for param in contract.functions[function_name].abi['inputs']]
-            txn = getattr(contract.functions, function_name)(*sorted_inputs).transact({'value': value})
-        else:
-            txn = getattr(contract.functions, function_name)().transact({'value': value})
-        tx_receipt = w3.eth.wait_for_transaction_receipt(txn)
-        print(f"Transação {function_name} executada com sucesso: {tx_receipt.transactionHash.hex()}")
-        return tx_receipt
-    except Exception as e:
-        print(f"Erro ao executar a transação {function_name}: {e}")
-        return None
-
-def genetic_fuzzer(abi, contract_instance, generations=10, population_size=5, mutation_rate=0.1):
-    population = [generate_random_inputs(abi, contract_instance) for _ in range(population_size)]
+def genetic_fuzzer(w3, abi, contract_instance, generations=10, population_size=5, mutation_rate=0.1):
+    population = [generate_random_inputs(abi) for _ in range(population_size)]
+ 
     for generation in range(generations):
         print(f"\nGeneration {generation}...")
         for inputs in population:
             for func in inputs:
                 func_name = func['name']
-                func_inputs = func['inputs']
+                func_inputs = func['inputs'] if len(func['inputs']) > 0 else None # Always empty for EtherStore!
+                func_state = func['stateMutability']
                 value = 0
-                if func_name == 'deposit':
-                    value = random.randint(1, 10**18)  # Deposita entre 1 wei e 1 ether
-                tx_receipt = simulate_transaction(contract_instance, func_name, func_inputs, value)
-                if tx_receipt:
-                    pass
+                
+                if func_state == 'payable':
+                    value = random.randint(1, 10**18)  # Deposit between 1 wei and 1 ether
+                    print(f"Transaction `{func_name}` received random input value: {value}")
+                tx_receipt = simulate_transaction(w3, contract_instance, func_name, func_inputs, value)
+        
         # Mutação da população
-        new_population = []
-        for _ in range(population_size):
-            mutated = mutate_inputs(random.choice(population))
-            new_population.append(mutated)
-        population = new_population
+        # new_population = []
+        # for _ in range(population_size):
+        #     mutated = mutate_inputs(random.choice(population))
+        #     new_population.append(mutated)
+        # population = new_population
 
-def mutate_inputs(inputs):
-    mutated_inputs = []
-    for func in inputs:
-        new_func = {'name': func['name'], 'inputs': {}}
-        for key, value in func['inputs'].items():
-            if isinstance(value, int):
-                new_value = random.randint(0, 2**256 - 1)
-            elif isinstance(value, str) and value.startswith('0x'):
-                new_value = '0x' + ''.join(random.choices('0123456789abcdef', k=len(value) - 2))
-            elif isinstance(value, str):
-                new_value = ''.join(random.choices('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ', k=random.randint(5, 15)))
-            elif isinstance(value, bool):
-                new_value = not value
-            else:
-                new_value = value  # Para tipos não suportados
-            new_func['inputs'][key] = new_value
-        mutated_inputs.append(new_func)
-    return mutated_inputs
+# def mutate_inputs(inputs):
+#     mutated_inputs = []
+#     for func in inputs:
+#         new_func = {'name': func['name'], 'inputs': {}}
+#         for key, value in func['inputs'].items():
+#             if isinstance(value, int):
+#                 new_value = random.randint(0, 2**256 - 1)
+#             elif isinstance(value, str) and value.startswith('0x'):
+#                 new_value = '0x' + ''.join(random.choices('0123456789abcdef', k=len(value) - 2))
+#             elif isinstance(value, str):
+#                 new_value = ''.join(random.choices('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ', k=random.randint(5, 15)))
+#             elif isinstance(value, bool):
+#                 new_value = not value
+#             else:
+#                 new_value = value  # Para tipos não suportados
+#             new_func['inputs'][key] = new_value
+#         mutated_inputs.append(new_func)
+#     return mutated_inputs
 
-# Nome do arquivo Solidity a ser compilado
-source_code_file = "EtherStorev2.sol"
 
-# Leitura do código fonte Solidity
-with open(source_code_file, 'r') as file:
-    source_code = file.read()
+if __name__ == "__main__":
+    blockhain_url = "http://127.0.0.1:8545"
+    contract_filename = "EtherStorev2.sol"
+    contract_name = "EtherStore"
+    solc_version = "0.8.24"
 
-# Configurações do compilador Solidity
-solc_version = "0.8.24"
-if solc_version not in solcx.get_installed_solc_versions():
-    solcx.install_solc(solc_version)
-solcx.set_solc_version(solc_version, True)
+    with open(contract_filename, 'r') as file:
+        source_code = file.read()
+    
+    compiler_output = compile_smartcontract(solc_version, contract_filename, source_code)
+    
+    # Smart contract information
+    contract_interface = compiler_output['contracts'][contract_filename][contract_name]
+    abi = contract_interface['abi']
+    bytecode = contract_interface['evm']['bytecode']['object']
+    
+    w3_conn = connect_in_blockchain(blockhain_url)
+    if w3_conn is not None:
+        ether_store_contract = deploy_smartcontract(w3_conn, abi, bytecode)
+    
+    # Optional step, just for checking
+    print("Depositing 1 Ether...")
+    tx_receipt = simulate_transaction(w3=w3_conn, contract=ether_store_contract, function_name='deposit', value=Web3.to_wei(1, 'ether'))
+    
+    # Getting low-level calls information
+    result = w3_conn.manager.request_blocking('debug_traceTransaction', [f"0x{tx_receipt.transactionHash.hex()}"])
+    result = dict(result)
 
-# Compilação do código Solidity
-compiler_output = solcx.compile_standard({
-    'language': 'Solidity',
-    'sources': {source_code_file: {'content': source_code}},
-    'settings': {
-        "optimizer": {"enabled": True, "runs": 200},
-        "evmVersion": "cancun",
-        "outputSelection": {
-            source_code_file: {
-                "*": [
-                    "abi",
-                    "evm.deployedBytecode",
-                    "evm.bytecode.object",
-                    "evm.legacyAssembly",
-                ],
-            }
-        }
-    }
-}, allow_paths='.')
+    # Formatting output
+    temp_logs = []
+    for log in result["structLogs"]:
+        temp_log = dict(log)
+        temp_log["storage"] = dict(temp_log["storage"])
+        temp_logs.append(temp_log)
+    result["structLogs"] = temp_logs
+    
+    # Saving low-level calls to .json
+    with open('result.json', 'w') as fp:
+        json.dump(result, fp)
+    print("Low-level calls saved to .json file!")
+    
+    # if tx_receipt is not None:
+    #     print("Contract balance: {}".format(ether_store_contract.functions.getBalance().call()))
+    #     genetic_fuzzer(w3_conn, abi, ether_store_contract) # Init Fuzzing proccess
 
-# Implementação do contrato
-contract_name = "EtherStore"
-contract_interface = compiler_output['contracts'][source_code_file][contract_name]
-abi = contract_interface['abi']
-bytecode = contract_interface['evm']['bytecode']['object']
-
-# Implantando o contrato
-transaction = {
-    'from': w3.eth.default_account,
-    'gas': 2000000,
-    'gasPrice': w3.to_wei('50', 'gwei')
-}
-
-# Enviando a transação para implantar o contrato
-contract = w3.eth.contract(abi=abi, bytecode=bytecode)
-tx_hash = contract.constructor().transact(transaction)
-tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
-
-# Obter o endereço do contrato implantado
-contract_address = tx_receipt['contractAddress']
-print(f"Contrato implantado em: {contract_address}")
-
-# Criando uma instância do contrato implantado
-ether_store_contract = w3.eth.contract(address=contract_address, abi=abi)
-
-# Opcional: Realizar um depósito inicial para evitar erros de saldo insuficiente
-initial_deposit = 10**18  # 1 Ether
-print("Realizando depósito inicial de 1 Ether...")
-tx_receipt = simulate_transaction(ether_store_contract, 'deposit', {}, initial_deposit)
-if tx_receipt:
-    print("Depósito inicial realizado com sucesso.")
-
-# Inicia o fuzzing
-genetic_fuzzer(abi, ether_store_contract)  # Inicia o processo de fuzzing
