701512ee353e2c3767e55e6babb665957250aba0
aline <145055918+alinec-santos@users.noreply.github.com>
2024-11-06 21:52:23 -0300

    Code Coverage and Detection Reentrancy

--- tests_aline/tests_aline.py
+++ tests_aline/tests_aline.py
@@ -1,14 +1,34 @@
-import json
 import solcx
-import random
 from web3 import Web3
+import random, pprint
+import json
 
-# Função para obter PCs e instruções de salto no bytecode
-def get_pcs_and_jumpis(bytecode):
-    pcs = [i for i in range(len(bytecode))]
-    jumpis = [i for i in range(len(bytecode)) if bytecode[i:i+2] == '56']  # '56' é um exemplo de opcode JUMPI
-    return pcs, jumpis
+def compile_smartcontract(compiler_version, contract_filename, source_code):
+    if compiler_version not in solcx.get_installed_solc_versions():
+        solcx.install_solc(compiler_version)
+    solcx.set_solc_version(solc_version, True)
 
+    compiler_output = solcx.compile_standard({
+        'language': 'Solidity',
+        'sources': {contract_filename: {'content': source_code}},
+        'settings': {
+            "optimizer": {"enabled": True, "runs": 200},
+            "evmVersion": "cancun",
+            "outputSelection": {
+                contract_filename: {
+                    "*": [
+                        "abi",
+                        "evm.deployedBytecode",
+                        "evm.bytecode.object",
+                        "evm.legacyAssembly",
+                    ],
+                }
+            }
+        }
+    }, allow_paths='.')
+    print("Smart contract compiled!")
+
+    return compiler_output
 # Classe que carrega o conteúdo do código fonte Solidity
 class Source:
     def __init__(self, filename):
@@ -81,112 +101,90 @@ class SourceMap:
     def get_buggy_line(self, pc):
         try:
             pos = self.instr_positions[pc]
+            # Calcular o número da linha baseado na posição
+            line_number = sum(1 for i in self.source.line_break_positions if i < pos['begin']) + 1
+            line_content = self.source.content[pos['begin']:pos['end']].strip()
+            return line_number, line_content
         except KeyError:
-            return ""
-        begin = pos['begin']
-        end = pos['end']
-        return self.source.content[begin:end]
+            return -1, ""  # Retorna -1 e uma string vazia se não encontrar a posição
+
+def connect_in_blockchain(url):
+    w3 = Web3(Web3.HTTPProvider(url))
+    if w3.is_connected():
+        print("Blockchain connected succesfully!")
+        w3.eth.default_account = w3.eth.accounts[0]
+        return w3
+    
+    print("Error during the connection with the blockchain!")
+    return None
+
+def deploy_smartcontract(w3, abi, bytecode):
+    smart_contract = w3.eth.contract(abi=abi, bytecode=bytecode)
+    tx_hash = smart_contract.constructor().transact()
+    tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
+    contract_address = tx_receipt.contractAddress
+    print(f"Contract Address: {contract_address}")
+    
+    return w3.eth.contract(address=contract_address, abi=abi)
+
+def get_pcs_and_jumpis(bytecode):
+    pcs = [i for i in range(len(bytecode))]
+    jumpis = [i for i in range(len(bytecode)) if bytecode[i:i+2] == '56']  # Example: '56' is JUMPI opcode
+    return pcs, jumpis
+
 
 # Função para obter a linha do código-fonte a partir do PC
 def get_source_line_from_pc(pc, source_map):
     line = source_map.get_buggy_line(pc) #busca a linha correspondente ao pc dentro de source_map.
     return line.strip() if line else "Linha não encontrada"
 
-# Função para compilar o contrato
-def compile_smartcontract(solc_version, filename, source_code):
-    if solc_version not in solcx.get_installed_solc_versions():
-        solcx.install_solc(solc_version)
-    solcx.set_solc_version(solc_version, True)
+#code coverage
+def code_coverage(logs):
+    """ Track PCs hit during transaction for code coverage analysis. """
+    covered_pcs = set()
+    for log in logs:
+        if "pc" in log:
+            covered_pcs.add(log["pc"])
+    return covered_pcs
 
-    return solcx.compile_standard({
-        'language': 'Solidity',
-        'sources': {filename: {'content': source_code}},
-        'settings': {
-            "optimizer": {"enabled": True, "runs": 200},
-            "evmVersion": "cancun",
-            "outputSelection": {
-                filename: {
-                    "*": [
-                        "abi",
-                        "evm.deployedBytecode",
-                        "evm.bytecode.object",
-                        "evm.legacyAssembly",
-                    ],
-                }
-            }
-        }
-    })
+def update_coverage(coverage_map, new_coverage):
+    """ Update the coverage map with new transaction coverage """
+    for pc in new_coverage:
+        if pc not in coverage_map:
+            coverage_map[pc] = 1
+        else:
+            coverage_map[pc] += 1
+    return coverage_map
 
-# Função para conectar à blockchain
-def connect_in_blockchain(url):
-    w3 = Web3(Web3.HTTPProvider(url))
-    if w3.is_connected():
-                print("Conectado à blockchain.")
-                return w3
-    else:
-        print("Falha na conexão com a blockchain.")
-        return None
+def calculate_coverage(coverage_map, total_pcs):
+    """ Calculate percentage of code covered based on unique PCs """
+    unique_pcs_covered = len(coverage_map.keys())
+    coverage_percentage = (unique_pcs_covered / total_pcs) * 100
+    print(f"Current Code Coverage: {coverage_percentage:.2f}%")
+    return coverage_percentage
 
-# Função para implantar o contrato
-def deploy_smartcontract(w3, abi, bytecode):
-    contract = w3.eth.contract(abi=abi, bytecode=bytecode)
-    tx_hash = contract.constructor().transact()
-    tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
-    print(f"Contrato implantado em: {tx_receipt.contractAddress}")
-    return contract(tx_receipt.contractAddress)
+    
 
-
-def detect_reentrancy(sloads, calls, current_instruction, source_map):
-    if current_instruction["op"] == "SLOAD":
-        storage_index = current_instruction["stack"][-1]
-        sloads[storage_index] = current_instruction["pc"]
-    elif current_instruction["op"] == "CALL" and sloads:
-        gas = int(current_instruction["stack"][-1], 16)
-        value = int(current_instruction["stack"][-3], 16)
-
-        if gas > 2300 and value > 0:
-            calls.add(current_instruction["pc"])
-            for pc in sloads.values():
-                if pc < current_instruction["pc"]:
-                    # Aqui você pode obter a linha de código correspondente
-                    buggy_line = get_source_line_from_pc(pc, source_map)
-                    print(f"Reentrância detectada em CALL na linha: {buggy_line.strip()}")
-                    return current_instruction["pc"]
-    elif current_instruction["op"] == "SSTORE" and calls:
-        storage_index = current_instruction["stack"][-1]
-        if storage_index in sloads:
-            for pc in calls:
-                if pc < current_instruction["pc"]:
-                    buggy_line = get_source_line_from_pc(pc, source_map)
-                    print(f"Reentrância detectada em SSTORE na linha: {buggy_line.strip()}")
-                    return pc
-    elif current_instruction["op"] in ["STOP", "RETURN", "REVERT", "ASSERTFAIL", "INVALID", "SUICIDE", "SELFDESTRUCT"]:
-        sloads.clear()
-        calls.clear()
-    return None
-
-# Simulação de transação 
 def simulate_transaction(w3, contract, function_name, inputs=None, value=0):
     try:
         if inputs:
+            # Sorting inputs accordingly with function parameters
             sorted_inputs = [inputs[param['name']] for param in contract.functions[function_name].abi['inputs']]
             txn = getattr(contract.functions, function_name)(*sorted_inputs).transact({'value': value})
         else:
             txn = getattr(contract.functions, function_name)().transact({'value': value})
-
         tx_receipt = w3.eth.wait_for_transaction_receipt(txn)
-        print(f"Transaction '{function_name}' executada com sucesso: {tx_receipt.transactionHash.hex()}")
+        print(f"Transaction '{function_name}' executed successfully: {tx_receipt.transactionHash.hex()}")
         return tx_receipt
     except Exception as e:
-        print(f"Erro durante a execução da transação '{function_name}': {e}")
+        print(f"Error during transaction '{function_name}' execution: {e}")
         return None
 
-# Função para gerar entradas aleatórias para fuzzing
 def generate_random_inputs(abi):
     inputs = []
     
     for item in abi:
-        if item['type'] == 'function' and item['name'] != 'balances':  # Apenas para EtherStore!
+        if item['type'] == 'function' and item['name'] != 'balances': # Only for EtherStore!
             function_inputs = dict()
             for input_param in item.get('inputs', []):
                 param_type = input_param['type']
@@ -202,7 +200,7 @@ def generate_random_inputs(abi):
                     size = int(param_type.replace('bytes', '')) if len(param_type) > 5 else random.randint(1, 32)
                     value = '0x' + ''.join(random.choices('0123456789abcdef', k=size*2))
                 else:
-                    value = None  # Tipos não suportados podem ser ignorados ou tratados conforme necessário
+                    value = None # Non-supported types can be ignored or treated as needed
                 if value is not None:
                     function_inputs[input_param['name']] = value
             inputs.append({
@@ -213,49 +211,97 @@ def generate_random_inputs(abi):
 
     return inputs
 
-# Função para salvar chamadas de baixo nível em um arquivo JSON
 def save_lowlevelcalls(result, out_filename):
     result = dict(result)
-    # Formatando saída
+    # Formatting output
     temp_logs = []
     for log in result["structLogs"]:
         temp_log = dict(log)
         temp_log["storage"] = dict(temp_log["storage"])
         temp_logs.append(temp_log)
     result["structLogs"] = temp_logs
-    # Salvando chamadas de baixo nível em .json
+    # Saving low-level calls to .json
     with open(out_filename, 'w') as fp:
         json.dump(result, fp)
 
-# Função de fuzzing genético
-def genetic_fuzzer(w3, abi, contract_instance, sloads, calls, source_map, generations=1, population_size=1):
+def genetic_fuzzer(w3, abi, contract_instance, sloads, calls,source_map, generations=1, population_size=1):
     population = [generate_random_inputs(abi) for _ in range(population_size)]
+    coverage_map = {}
+    total_pcs = len(source_map.instr_positions)#all contract pcs
+
  
     for generation in range(generations):
         print(f"\nGeneration {generation}...")
         for inputs in population:
             for func in inputs:
                 func_name = func['name']
-                func_inputs = func['inputs'] if len(func['inputs']) > 0 else None  # Sempre vazio para EtherStore!
+                func_inputs = func['inputs'] if len(func['inputs']) > 0 else None # Always empty for EtherStore!
                 func_state = func['stateMutability']
                 value = 0
                 
                 if func_state == 'payable':
-                    value = random.randint(1, 10**18)  # Depósito entre 1 wei e 1 ether
-                    print(f"Transação `{func_name}` recebeu valor de entrada aleatório: {value}")
+                    value = random.randint(1, 10**18)  # Deposit between 1 wei and 1 ether
+                    print(f"Transaction `{func_name}` received random input value: {value}")
                 tx_receipt = simulate_transaction(w3, contract_instance, func_name, func_inputs, value)
                 
-                # Verificar instruções
+                # Check instructions
                 result = w3.manager.request_blocking('debug_traceTransaction', [f"0x{tx_receipt.transactionHash.hex()}"])
+                logs = result["structLogs"] if "structLogs" in result else []
+                new_coverage = code_coverage(logs)
+                update_coverage(coverage_map, new_coverage)
+                    
                 save_lowlevelcalls(result, f"gen{generation}_{func_name}.json")
                 if not result.failed:
                     for i, instruction in enumerate(result.structLogs):
-                        pc = detect_reentrancy(sloads, calls, instruction, source_map)  # Passando source_map
-                        if pc:
-                            print(f"Reentrância detectada em {func_name}:", pc)
-# Função principal
+                        pc = detect_reentrancy(sloads, calls, instruction)
+                        #if pc:
+                            #print(f"Detected reentrancy in {func_name}:", pc)
+        #calculate the coverage from all the code
+        calculate_coverage(coverage_map, total_pcs)
+def detect_reentrancy(sloads, calls, current_instruction):
+    # Lembrar sloads
+    if current_instruction["op"] == "SLOAD":
+        storage_index = current_instruction["stack"][-1]
+        sloads[storage_index] = current_instruction["pc"]
+    # Lembrar chamadas com mais de 2300 gas e onde o valor é maior que zero/simbólico ou onde o destino é simbólico
+    elif current_instruction["op"] == "CALL" and sloads:
+        gas = int(current_instruction["stack"][-1], 16)  # Gás da instrução
+        value = int(current_instruction["stack"][-3], 16)  # Saldo atual do contrato
+
+        if gas > 2300 and value > 0:
+            calls.add(current_instruction["pc"])
+            for pc in sloads.values():
+                if pc < current_instruction["pc"]:
+                    # Aqui você pode obter o número da linha e o conteúdo correspondente
+                    line_number, line_content = source_map.get_buggy_line(pc)
+                    if line_number != -1:
+                        print(f"Reentrância detectada na linha {line_number}: {line_content}")
+                    return current_instruction["pc"]
+
+    elif current_instruction["op"] == "SSTORE" and calls:
+        storage_index = current_instruction["stack"][-1]
+        if storage_index in sloads:
+            for pc in calls:
+                if pc < current_instruction["pc"]:
+                    line_number, line_content = source_map.get_buggy_line(pc)
+                    if line_number != -1:
+                        print(f"Reentrância detectada na linha {line_number}: {line_content}")
+                    return pc
+    # Limpa sloads e chamadas de transações anteriores
+    elif current_instruction["op"] in ["STOP", "RETURN", "REVERT", "ASSERTFAIL", "INVALID", "SUICIDE", "SELFDESTRUCT"]:
+        sloads.clear()
+        calls.clear()
+    return None  # NÃO FOI ENCONTRADA REENTRADA!
+
+def save_source_map(compiler_output, contract_filename, contract_name, out_filename):
+    source_map = compiler_output['contracts'][contract_filename][contract_name]['evm']['deployedBytecode'].get('sourceMap', '')
+    
+    # saves the sourcemap in a json file
+    with open(out_filename, 'w') as file:
+        json.dump({"sourceMap": source_map}, file, indent=4)
+
 if __name__ == "__main__":
-    blockchain_url = "http://127.0.0.1:8545"
+    blockhain_url = "http://127.0.0.1:8545"
     contract_filename = "EtherStorev2.sol"
     contract_name = "EtherStore"
     solc_version = "0.8.24"
@@ -263,32 +309,28 @@ if __name__ == "__main__":
     sloads = dict()
     calls = set()
 
-    # Ler o código do contrato
     with open(contract_filename, 'r') as file:
         source_code = file.read()
     
-    # Compilar o contrato
     compiler_output = compile_smartcontract(solc_version, contract_filename, source_code)
     
-    # Obter informações do contrato
+    # Smart contract information
     contract_interface = compiler_output['contracts'][contract_filename][contract_name]
     abi = contract_interface['abi']
     bytecode = contract_interface['evm']['bytecode']['object']
+    deployed_bytecode = contract_interface['evm']['deployedBytecode']['object']
     
-    # Conexão com a blockchain e deploy do contrato
-    w3_conn = connect_in_blockchain(blockchain_url)
+    # Connection and deploy
+    w3_conn = connect_in_blockchain(blockhain_url)
     if w3_conn is not None:
         ether_store_contract = deploy_smartcontract(w3_conn, abi, bytecode)
     
-    # Criar uma instância do SourceMap
-    source_map = SourceMap(f"{contract_filename}:{contract_name}", compiler_output)
-
-    # Realizar um depósito como exemplo
-    print("Depositando 1 Ether...")
+    # Optional step, just for checking
+    print("Depositing 1 Ether...")
     tx_receipt = simulate_transaction(w3=w3_conn, contract=ether_store_contract, function_name='deposit', value=Web3.to_wei(1, 'ether'))
-    
+
+    source_map = SourceMap(f"{contract_filename}:{contract_name}", compiler_output)
+    save_source_map(compiler_output, contract_filename, contract_name, 'source_map.json')
     if tx_receipt is not None:
-        print("Saldo do contrato: {}".format(ether_store_contract.functions.getBalance().call()))
-        
-        # Chamada da função genetic_fuzzer com os parâmetros adicionais
-        genetic_fuzzer(w3_conn, abi, ether_store_contract, sloads, calls, source_map, generations=1, population_size=1)  # Init Fuzzing process
+        print("Contract balance: {}".format(ether_store_contract.functions.getBalance().call()))
+        genetic_fuzzer(w3_conn, abi, ether_store_contract, sloads, calls,source_map) # Init Fuzzing proccess
